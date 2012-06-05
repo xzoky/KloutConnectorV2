9148c86927dd84a90fa513edcc072585b286fdb2
Camille Kander <camille@kander.com>
2012-06-05 18:04:40 +0200

    Initial Commit

---
+++ KloutConnectorV2/KloutConnectorV2.m
@@ -0,0 +1,13 @@
+//
+//  KloutConnectorV2.m
+//  KloutConnectorV2
+//
+//  Created by Camille Kander on 05/06/12.
+//  Copyright (c) 2012 Supinfo. All rights reserved.
+//
+
+#import "KloutConnectorV2.h"
+
+@implementation KloutConnectorV2
+
+@end
---
+++ KloutConnectorV2Tests/KloutConnectorV2Tests.h
@@ -0,0 +1,13 @@
+//
+//  KloutConnectorV2Tests.h
+//  KloutConnectorV2Tests
+//
+//  Created by Camille Kander on 05/06/12.
+//  Copyright (c) 2012 Supinfo. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface KloutConnectorV2Tests : SenTestCase
+
+@end
